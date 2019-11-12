a51c3763756b539f036242a3a7ec7fd4bca378e5
Student <example@example.com>
2019-11-12 11:34:37 -0800

    fix problems with webpack and create entry object

---
+++ src/backend.js
@@ -0,0 +1,7 @@
+export function Entry (body) {
+  this.body = body
+  this.words = []
+  this.letters = []
+  this.vowelsNumber = 0
+  this.consonantsNumber = 0
+}
--- src/frontend.js
+++ src/frontend.js
@@ -1,1 +1,13 @@
-import "./styles.css";
+import { Entry } from './backend.js';
+import './styles.css';
+
+$(document).ready(function(){
+  $("#userForm").submit(function(e){
+    e.preventDefault();
+    var userEntry = $("#userEntry").val();
+    var userJournal = new Entry(userEntry)
+    console.log(userJournal);
+  })
+
+
+});
