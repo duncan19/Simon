5aa1a0850e0c46af05917ff658131da2f5fcba03
Student <example@example.com>
2019-11-12 13:48:39 -0800

    initial commit

--- src/frontend.js
+++ src/frontend.js
@@ -1,24 +1,5 @@
-import { Entry } from './backend.js';
+
 import './styles.css';
-
 $(document).ready(function(){
-  $("#userForm").submit(function(e){
-    e.preventDefault();
-    var userEntry = $("#userEntry").val();
-    var userJournal = new Entry(userEntry)
-    userJournal.splitWords();
-    userJournal.numberOfWords();
-    userJournal.numberOfVowels();
-    userJournal.numberOfConsonants();
-    userJournal.teaser8();
-    $("#teaser").text(userJournal.teaser);
-    $("#entry").text(userJournal.body);
-    $("#words").text(userJournal.wordsNumber);
-    $("#vowels").text(userJournal.vowelsNumber);
-    $("#consonants").text(userJournal.consonantsNumber);
-    $(".results").show();
-
-  })
-
 
 });
